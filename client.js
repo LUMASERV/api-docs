de79d8424643a3b7faad6c80b91834f3d5780844
JanHolger <jan@bebendorf.eu>
2021-08-16 21:54:23 +0000

    Update build to output generated at 9953261

--- client.js
+++ client.js
@@ -51,16 +51,118 @@ export default class LUMASERVClient {
     createProject(body = null){
         return this.request('post', '/projects', null, body);
     }
-    deleteProject(uuid){
-        return this.request('delete', '/projects/' + uuid, null, null);
+    deleteProject(id){
+        return this.request('delete', '/projects/' + id, null, null);
     }
-    getProject(uuid){
-        return this.request('get', '/projects/' + uuid, null, null);
+    getProject(id){
+        return this.request('get', '/projects/' + id, null, null);
     }
-    updateProject(uuid, body = null){
-        return this.request('put', '/projects/' + uuid, null, body);
+    updateProject(id, body = null){
+        return this.request('put', '/projects/' + id, null, body);
     }
     login(body = null){
         return this.request('post', '/auth/login', null, body);
+    }
+    getUsers(){
+        return this.request('get', '/users', null, null);
+    }
+    getUser(id){
+        return this.request('get', '/users/' + id, null, null);
+    }
+    getS3Buckets(){
+        return this.request('get', '/storage/s3/buckets', null, null);
+    }
+    createS3Bucket(body = null){
+        return this.request('post', '/storage/s3/buckets', null, body);
+    }
+    deleteS3Bucket(id){
+        return this.request('delete', '/storage/s3/buckets/' + id, null, null);
+    }
+    getS3Bucket(id){
+        return this.request('get', '/storage/s3/buckets/' + id, null, null);
+    }
+    getS3AccessKeys(){
+        return this.request('get', '/storage/s3/access-keys', null, null);
+    }
+    createS3AccessKey(body = null){
+        return this.request('post', '/storage/s3/access-keys', null, body);
+    }
+    deleteS3AccessKey(id){
+        return this.request('delete', '/storage/s3/access-keys/' + id, null, null);
+    }
+    getS3AccessKey(id){
+        return this.request('get', '/storage/s3/access-keys/' + id, null, null);
+    }
+    getPleskLicenses(){
+        return this.request('get', '/licenses/plesk', null, null);
+    }
+    createPleskLicense(body = null){
+        return this.request('post', '/licenses/plesk', null, body);
+    }
+    getPleskLicense(id){
+        return this.request('get', '/licenses/plesk/' + id, null, null);
+    }
+    updatePleskLicense(id, body = null){
+        return this.request('put', '/licenses/plesk/' + id, null, body);
+    }
+    getPleskLicenseTypes(){
+        return this.request('get', '/licenses/plesk-types', null, null);
+    }
+    getPleskLicenseType(id){
+        return this.request('get', '/licenses/plesk-types/' + id, null, null);
+    }
+    getS3AccessKeyGrants(access_key_id){
+        return this.request('get', '/storage/s3/access-keys/' + access_key_id + '/grants', null, null);
+    }
+    createS3AccessKeyGrant(access_key_id, body = null){
+        return this.request('post', '/storage/s3/access-keys/' + access_key_id + '/grants', null, body);
+    }
+    deleteS3AccessKeyGrant(access_key_id, id){
+        return this.request('delete', '/storage/s3/access-keys/' + access_key_id + '/grants/' + id, null, null);
+    }
+    getDomainHandles(){
+        return this.request('get', '/domain-handles', null, null);
+    }
+    createDomainHandle(body = null){
+        return this.request('post', '/domain-handles', null, body);
+    }
+    deleteDomainHandle(code){
+        return this.request('delete', '/domain-handles/' + code, null, null);
+    }
+    getDomainHandle(code){
+        return this.request('get', '/domain-handles/' + code, null, null);
+    }
+    updateDomainHandle(code, body = null){
+        return this.request('put', '/domain-handles/' + code, null, body);
+    }
+    getDomains(){
+        return this.request('get', '/domains', null, null);
+    }
+    createDomain(body = null){
+        return this.request('post', '/domains', null, body);
+    }
+    deleteDomain(name){
+        return this.request('delete', '/domains/' + name, null, null);
+    }
+    getDomain(name){
+        return this.request('get', '/domains/' + name, null, null);
+    }
+    updateDomain(name, body = null){
+        return this.request('put', '/domains/' + name, null, body);
+    }
+    removeDomainAuthinfo(name){
+        return this.request('delete', '/domains/' + name + '/authinfo', null, null);
+    }
+    getDomainAuthinfo(name){
+        return this.request('get', '/domains/' + name + '/authinfo', null, null);
+    }
+    restoreDomain(name){
+        return this.request('post', '/domains/' + name + '/restore', null, null);
+    }
+    scheduleDomainDelete(name, body = null){
+        return this.request('post', '/domains/' + name + '/schedule-delete', null, body);
+    }
+    unscheduleDomainDelete(name){
+        return this.request('post', '/domains/' + name + '/unschedule-delete', null, null);
     }
 }
