dc2fd03365f14ba3583d52b6872b9fe932083c5e
jan@bebendorf.eu <jan@bebendorf.eu>
2022-06-29 23:43:21 +0200

    Modified 5 files

--- client.source.js
+++ client.source.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default class LUMASERV{API_NAME}Client {
-    constructor(baseURL = 'https://api.lumaserv.cloud') {
+    constructor(baseURL = 'https://api.lumaserv.com') {
         this.axios = axios.create({
             baseURL,
             headers: {
