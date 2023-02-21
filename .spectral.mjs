b9512d26fd5b177c6a96857c70efc48e688e434b
jan@bebendorf.eu <jan@bebendorf.eu>
2023-02-21 01:57:50 +0100

    Modified 17 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE0NTU5/spectral.js?branch_slug=master&token=8416b598-b186-4c77-813c-9c51fd63b876";
+export default { extends: ruleset };
